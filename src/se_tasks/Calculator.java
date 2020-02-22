9a275947c028b36705a683bb2b5b1dc6ded6373f
King Kong <cuteh@LAPTOP-QM0H46RJ.mynet>
2020-02-22 08:45:19 +0200

    v4

---
+++ src/se_tasks/Calculator.java
@@ -0,0 +1,15 @@
+package se_tasks;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int sum(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+}
